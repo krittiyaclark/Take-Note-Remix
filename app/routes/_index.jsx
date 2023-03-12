6ae0c2fc34b09f22f1022dec21edd23174c9c754
krittiyaclark <krittiyaclark@gmail.com>
2023-03-12 16:20:18 -0400

    Added routes and styles

---
+++ app/routes/_index.jsx
@@ -0,0 +1,18 @@
+import { Link } from "@remix-run/react";
+import homeStyles from "~/styles/home.css";
+
+export default function Index() {
+  return (
+    <main id="content">
+      <h1>Note Taking App</h1>
+      <p>Don't loose track of your note agin!</p>
+      <p id="cta">
+        <Link to="/notes">Try Now!</Link>
+      </p>
+    </main>
+  );
+}
+
+export function links() {
+  return [{ rel: "stylesheet", href: homeStyles }];
+}
